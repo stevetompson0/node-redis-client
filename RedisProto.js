3ebfa7834ac5592cac874cac2864e14ddb8ef68e
steve xu <stevexucityu@gmail.com>
2016-05-17 17:36:04 -0700

    add airbnb lint

--- RedisProto.js
+++ RedisProto.js
@@ -2,4 +2,17 @@
  * Created by steve on 2016-05-17.
  */
 
-class
+/**
+ * RedisProto -- a class used to interpret responses from Redis server
+ */
+class RedisProto {
+
+  constructor() {
+    self.text = '';  // Partial command to be concatenated with the next input
+    self.lines = [];  // Commands that have been interpreted
+  }
+
+
+}
+
+module.exports = RedisProto;
